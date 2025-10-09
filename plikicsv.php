99dda43fcb5b20a0cf12258f0e77810d77ed6e2d
dawid <zajacdawid202@gmail.com>
2025-10-09 11:16:05 +0200

    09.10.2025

---
+++ plikicsv.php
@@ -0,0 +1,16 @@
+<?php
+$row=1;
+if(($handle=fopen("test.csv","r")) !==FALSE) {
+    while(($data=fgetdcsv($handle,1000, ",") !==FALSE) {
+        $num=count($data)
+        echo "<p> $num fields in line $row: <br /></p>\n"
+        row++;
+        for ($c=0; $c<$num;$c++) {
+        echo $data[$c] ."<br/>\n";
+
+
+    }
+}
+    fclose($handle);
+    }
+    ?>
